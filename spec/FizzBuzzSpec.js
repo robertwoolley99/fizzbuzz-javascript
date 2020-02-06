10908b63a9b914df3419ac7fa8f1b0a32ea0df20
Robert Woolley <rob@robertwoolley.co.uk>
2020-02-06 16:49:28 +0000

    initial setup

---
+++ spec/FizzBuzzSpec.js
@@ -0,0 +1,53 @@
+describe("FizzBuzz", function() {
+  var result;
+
+  beforeEach(function() {
+    result = new FizzBuzz();
+  });
+
+  it("should return the number if not divisible by 5 or 3", function() {
+    expect(result.fizzbuzz(2)).toEqual(2);
+  });
+
+
+  // describe("when song has been paused", function() {
+  //   beforeEach(function() {
+  //     player.play(song);
+  //     player.pause();
+  //   });
+  //
+  //   it("should indicate that the song is currently paused", function() {
+  //     expect(player.isPlaying).toBeFalsy();
+  //
+  //     // demonstrates use of 'not' with a custom matcher
+  //     expect(player).not.toBePlaying(song);
+  //   });
+  //
+  //   it("should be possible to resume", function() {
+  //     player.resume();
+  //     expect(player.isPlaying).toBeTruthy();
+  //     expect(player.currentlyPlayingSong).toEqual(song);
+  //   });
+  // });
+  //
+  // // demonstrates use of spies to intercept and test method calls
+  // it("tells the current song if the user has made it a favorite", function() {
+  //   spyOn(song, 'persistFavoriteStatus');
+  //
+  //   player.play(song);
+  //   player.makeFavorite();
+  //
+  //   expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+  // });
+  //
+  // //demonstrates use of expected exceptions
+  // describe("#resume", function() {
+  //   it("should throw an exception if song is already playing", function() {
+  //     player.play(song);
+  //
+  //     expect(function() {
+  //       player.resume();
+  //     }).toThrowError("song is already playing");
+  //   });
+  // });
+});
---
+++ src/FizzBuzz.js
@@ -0,0 +1,18 @@
+function FizzBuzz() {
+}
+
+FizzBuzz.prototype.fizzbuzz = function(input) {
+  return input
+};
+
+// Player.prototype.resume = function() {
+//   if (this.isPlaying) {
+//     throw new Error("song is already playing");
+//   }
+//
+//   this.isPlaying = true;
+// };
+//
+// Player.prototype.makeFavorite = function() {
+//   this.currentlyPlayingSong.persistFavoriteStatus(true);
+// };
